09e96430f1241646fc1da4d65af7c4d28ed5f0f1
cklasson <chrillec@gmail.com>
2021-12-02 10:38:49 +0100

    lägger till formulärhantering med en funktion och en addeventlistener

--- script/my_script.js
+++ script/my_script.js
@@ -9,3 +9,19 @@ $(document).ready( function() {
                     );}
                     );}
                     );
+
+
+       function handleSubmit(event) {
+            event.preventDefault();
+                 
+             const data = new FormData(event.target);
+             
+                const value = Object.fromEntries(data.entries());
+                 
+                  /* value.topics = data.getAll("topics"); */  
+                 
+                     console.log({ value });
+                   }
+                 
+                   const form = document.querySelector("form");
+                   form.addEventListener("submit", handleSubmit);
