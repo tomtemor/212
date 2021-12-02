456867fd23926151fce8b7a9f038de2cf78aff74
cklasson <chrillec@gmail.com>
2021-12-02 11:50:18 +0100

    update

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.listen(3000);  // startar servern på port 3000
 
 console.log("Kör servern på localhost:3000"); // meddelar att  servern körs
 
-app.post("/skriva-fil", (req, res) => {
-    let meddelande = req.body.meddelande;
-    fs.readFile("kontakt-meddelande-7.json", function (err, data) {
+app.post("/write", (req, res) => {
+    let inlagg = req.body.inlagg;
+    fs.readFile("json_data2.json", function (err, data) {
         let json = JSON.parse(data);
        // console.log("json: " + json[0].name);
        // console.log("json.topic: " + json.topic);
        
        json.push({"topic": "skitämne", "name": "Tore Vretman", "email": "nomail@nono.com", "message": "jag skulle vilja jobba hos er!"});
        
-        fs.writeFile("kontakt-meddelande-10.json", JSON.stringify(json), function(err){
+        fs.writeFile("json_data2.json", JSON.stringify(json), function(err){
           if (err) throw err;
           console.log("Ändringar sparade till filen!"); 
         });
