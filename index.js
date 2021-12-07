714c356a874eb6a432dc287e492a1f6c46fd8709
cklasson <chrillec@gmail.com>
2021-12-07 09:40:00 +0100

    lite uppdateringar, fått det att funka istortsett

--- index.js
+++ index.js
@@ -2,60 +2,80 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const static_path = path.join(__dirname, "/publik");
-app.use(express.static(static_path)); // för att servern ska hitta
+// const static_path = path.join(__dirname, "/publik");
+// app.use(express.static(static_path)); // för att servern ska hitta
+app.use(express.static("publik"));
+app.listen(4000);  // startar servern på port 4000
+console.log("Kör servern på localhost:4000"); // meddelar att  servern körs
 
 // app.get("/",function(req,res) {  < hade så innan,  vad är skillnaden på denna och den nedan?
 app.get("/",(req,res) => {  
-
-    res.sendFile(static_path + "/exempel.html");   // visar html-sidan
-
+  // res.sendFile(static_path + "/exempel.html");   // visar html-sidan
+  res.sendFile(__dirname + "/exempel.html");
 });
 
-
-
-app.listen(3000);  // startar servern på port 3000
-
-console.log("Kör servern på localhost:3000"); // meddelar att  servern körs
 
 const fs = require("fs");
-const { threadId } = require("worker_threads");
+// const { threadId } = require("worker_threads");
 
-app.get("/hamta-data", (req, res) =>{
+app.get("/hamta-data", (req, res) => {
   console.log("mottog förfrågan från klienten");
-  res.send("Hej från servern");
+ // res.send("Hej från servern");
   fs.readFile("test.json", function(err, data) {
     if (err) throw err;
+    
     res.send(data);
-  }
+  });
 });
 
+app.get("/skriv-fran-mall", (req, res) => {
+  fs.readFile("exempel.html", function(err, data){
+      fs.readFile("test.json", function(err, minJson) {
+        console.log("Minj" + minJson);
+          let html = data.toString().replace(/ERSÄTT_MED_SERVERGENERERAT_INNEHÅLL/, minJson);
+          res.send(html);
+      });
+  });
+});
 
 // ny skriv till fil -kod 2021-12-06
 app.use(express.urlencoded({extended:true}));
-app.post("/skriv-fran-mall", (req,res) => {
 
-  fs.readFile("test.json", function (err, data) {
-      let json = JSON.parse(data);
+app.post("/skriv-fran-mall", (req, res) => {
+  fs.readFile("exempel.html", function(err, data){
+  fs.readFile("test.json", function (err, minJson) {
+      let json = JSON.parse(minJson);
       let nyPost = {
-        topic: "skitämne",
+        topic: "tåpiiic",
         name: req.body.name,
-        email: "nissehult@sunet.se",
+        email: "nissehult718@sunet.se",
         message: req.body.inlagg 
       };
 
      // console.log("json: " + json[0].name);
      // console.log("json.topic: " + json.topic);
      
-     console.log(nyPost);
+     //console.log(nyPost);
      json.push(nyPost);
      
       fs.writeFile("test.json", JSON.stringify(json), function(err){
         if (err) throw err;
         console.log("Ändringar sparade till filen!"); 
       });
+      let output = "";
+            for (post in json) {
+                for (attribut in post) {
+                    output += post[attribut] + " ";
+                }
+                output += "<br><br>";
+            }
+           // console.log("output: " + output); 
+            let html = data.toString().replace(/ERSÄTT_MED_SERVERGENERERAT_INNEHÅLL/, output);
+           // console.log("outp: " + output);
+            res.send(html);
     });
 
+  });
 });
 
 // gammal kod nedan * kanske använder
--- publik/javascript/klientscript.js
+++ publik/javascript/klientscript.js
@@ -1,16 +1,17 @@
-alert("Hej hej!");
+// alert("Hej hej!");
 
-window.onload = function(){
+window.onload = function(){ 
 // $(document).ready( function() {
+    /* TIDIG KOD FÖR ATT PRESENTERA INLÄGG,  ><< ! fungerar förmodligen inte för att data inte kan hämtas från icke publik mapp?
     console.log("test");
-    $.getJSON("json_data.json", function(data){
+    $.getJSON("test.json", function(data){
           $.each(data.post, function(){
                  $("ul").append("<li>Namn: " +this['name'] + 
                  "</li> <li>Inlägg: " + this['inlagg'] + "</li><li>" + "När: " + this['tid'] + "</li><br/>"
                  );}
                  );}
-                 );
-                 
+                 ); 
+                 */
 
 
 let forfragan = new XMLHttpRequest();
@@ -18,8 +19,19 @@ forfragan.open("GET", "/hamta-data");
 
 forfragan.onload = function(){
     console.log("mottaget svar från servern");
-    console.log(this);
-    console.log(this.response);
+   // console.log(this);
+   // console.log(this.response);
+    data = JSON.parse(this.response);
+  // console.log(data);
+ 
+   //for (let i = 0; i < data.length; i++) { // för vart och ett av objekten i fältet // tidigare kod
+   for (let i = data.length-1; i >=0; i--) { // för vart och ett av objekten i fältet // ny kod > baklänges
+   
+   for (attribut in data[i]) {
+        document.getElementById("output").innerHTML += data[i][attribut] + " ";
+    }
+    document.getElementById("output").innerHTML += "<br><br>";
+}
 
 }
 
