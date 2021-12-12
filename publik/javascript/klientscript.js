f64f0a2a41a85b3d53ebf36d232948055f5c9462
cklasson <chrillec@gmail.com>
2021-12-12 16:41:27 +0100

    lite nytt på valideringssidan av formuläret

--- publik/javascript/klientscript.js
+++ publik/javascript/klientscript.js
@@ -6,11 +6,28 @@ Har skapat en enkel gästbok och lyckats få det funka!
 Försökte få till lite mer funktionalitet men det blev för krångligt... Har grundläggande koden..
 */
 
-
-
-
-
 window.onload = function(){ 
+
+//  Validering av textarean i formuläret 
+      let button_button = document.getElementById("button");
+      button_button.disabled = true; // inaktivera button_buttonen från början
+      let mandatory = document.getElementsByClassName("mandato");
+      console.log(mandatory);
+     // for (let i = 0; i < mandatory.length; i++) { // loopa igenom alla (båda) formulärfälten
+          mandatory[0].addEventListener("input", function() {  // lägg till händelselyssnare till varje formulärfält; händelsen "input" triggas när användaren skriver eller raderar något i input-fältet
+              //console.log("Skriver i formuläret");
+              //console.log(mandatory[i].value);
+              button_button.disabled = false;    // aktivera button_buttonen
+              this.style = "border : solid green;";   // ge återkoppling till användaren att formulärfältet är korrekt
+           //   for (let j = 0; j < mandatory.length; j++) { // loopa igenom alla (båda) formulärfälten igen
+                  if (!mandatory[0].value) {
+                      button_button.disabled = true; // avaktivera button_buttonen
+                      mandatory[j].style = "border : solid red;";  // ge återkoppling till användaren att formulärfältet är fel infyllt
+                  }
+            //  }
+          });
+     // }
+    
 
 
 // Ajax för att hämta data
